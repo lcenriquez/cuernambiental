5c3b7db0690c1b38e3bb3b4e656acd8f5a3595cc
Luis Carlos Enríquez <lcenriquez@users.noreply.github.com>
2020-05-24 12:20:54 -0500

    Setup authentication with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'authentication#index'
-  get 'authentication/index'
-  get 'authentication/register'
+  root 'users#index'
+  devise_for :users
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
